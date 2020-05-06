af94196c32035906416a572a40b982a11adc322a
JeremyTaraba <jeremytaraba@gmail.com>
2020-05-05 21:07:37 -0700

    added explored checker

--- uniformCost.cpp
+++ uniformCost.cpp
@@ -3,8 +3,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <math.h>
 
 using namespace std;
+
+const int LEFT = 0;
+const int UP = 1;
+const int RIGHT = 2;
+const int DOWN = 3;
 
 //creates operator overloading for priority_queue
 struct compare { 
@@ -23,7 +29,7 @@ bool uniformCostSearch(vector<int> puzzle){
     priority_queue <linkedList*, vector<linkedList*>, compare> frontier;  
 
     //keep track of past puzzle states so we dont revisit them
-    vector<int> explored;   
+    vector<vector<int>> explored;   
 
      //generate goal state based on differing sizes of puzzles
      vector<int> goal;
@@ -41,26 +47,26 @@ bool uniformCostSearch(vector<int> puzzle){
      linkedList *parentNode = NULL;
      linkedList *childNode;
 
+     //first iteration of while loop creates a copy of root node.
+     //should create a deallocator function and manually delete root node so no leaks
+
     // while(1){
     //     if(frontier.empty()){
     //         return false;
     //     }
-    //     curNode = createNode(frontier.top(), 0, parentNode);
+    //     curNode = createNode(frontier.top()->state, 0, parentNode);
     //     frontier.pop();
-    //     if(goalChecker(curNode, goal)){
+    //     if(goalChecker(curNode->state, goal)){
     //         return true;
     //     }
-    //     explored.push(curNode);
+    //     explored.push_back(curNode->state);
     //     for(//each different move we can do based on 0){
-    //         childNode = ;//one of the next actions we can do
+    //         childNode = ;//one of the next states we can create
     //         //for loop to check if child node is in frontier or if it is in explored
     //         //else frontier.push(childNode);
     //         //else if in frontier but the one in frontier has a higher path cost then replace it with this childNode
     //     }
 
-
-
-    // }
 
     return true;
 }
@@ -69,8 +75,24 @@ void finalOutput(int expandNodes, int queueNodes){
     cout << "goal!" << endl;
     cout << "To solve this problem the search algorithm expanded a total of " << expandNodes << " nodes" << endl;
     cout << "The maximum number of nodes in the queue at any one time: " << queueNodes << endl;
-
 }
+
+bool exploredChecker(vector<int> curNode, vector<vector<int>> explored){
+    int exploreCounter = 0;     //keeps track of how many puzzles we checked
+
+    for(int i = 0; i < explored.size(); i++){
+        for(int j = 0; j < explored.at(i).size(); j++){
+            if(curNode.at(j) != explored.at(i).at(j)){
+                exploreCounter++;
+                break;
+            }
+        }
+    }
+
+    //we went through all puzzles in explored. if not equal then we had a match so return true
+    return exploreCounter != explored.size();
+}
+
 
 bool goalChecker(vector<int> puzzle, vector<int> goal){
     bool goalReached = true;
@@ -83,11 +105,85 @@ bool goalChecker(vector<int> puzzle, vector<int> goal){
     return goalReached;
 }
 
+int findLocationOfZero(vector<int> puzzle){
+    int moveCase = 1;
+
+    for(int i = 0; i < puzzle.size(); i++){
+        if(puzzle.at(i) == 0){
+            break;
+        }
+        moveCase++;
+    }
+
+    return moveCase;
+}
+
+vector<int> findMoves(int zeroLocation, int puzzleSize){
+    vector<int> possibleMoves;
+
+    if(zeroLocation == 1){          //means if 0 is in top left corner
+        possibleMoves.push_back(RIGHT);
+        possibleMoves.push_back(DOWN);
+    }
+    else if(zeroLocation == puzzleSize){       //if 0 is in bottom right corner
+        possibleMoves.push_back(LEFT);
+        possibleMoves.push_back(UP);
+    }
+    else if(zeroLocation == sqrt(puzzleSize)){     //if 0 is at the end of the first row
+        possibleMoves.push_back(LEFT);
+        possibleMoves.push_back(DOWN);
+    }
+    else if( (zeroLocation > 1) && (zeroLocation < sqrt(puzzleSize)) ){     //if 0 is between the top left corner and end of first row
+        possibleMoves.push_back(LEFT);
+        possibleMoves.push_back(DOWN);
+        possibleMoves.push_back(RIGHT);
+    }
+    else if(zeroLocation == (puzzleSize - sqrt(puzzleSize)) ){     //if 0 is at the start of the last row
+        possibleMoves.push_back(LEFT);
+        possibleMoves.push_back(DOWN);
+    }
+    else if( (zeroLocation > (puzzleSize - sqrt(puzzleSize)) ) && (zeroLocation < puzzleSize) ){     //if 0 is between the bottom right corner and bottom left corner
+        possibleMoves.push_back(LEFT);
+        possibleMoves.push_back(UP);
+        possibleMoves.push_back(RIGHT);
+    }
+    else{   //zero is not in the first or last rows and is somewhere in the rows between
+        for(int i = 1; i < (1-sqrt(puzzleSize)); i++){
+            if(zeroLocation == (i*sqrt(puzzleSize) + 1) ){    //if 0 is start of a row
+                possibleMoves.push_back(UP);
+                possibleMoves.push_back(RIGHT);
+                possibleMoves.push_back(DOWN);
+                break;
+            }
+            else if(zeroLocation == ((i+1)*sqrt(puzzleSize)) ){   //if 0 is end of a row
+                possibleMoves.push_back(UP);
+                possibleMoves.push_back(LEFT);
+                possibleMoves.push_back(DOWN);
+            }
+            else if(zeroLocation > (i*sqrt(puzzleSize) + 1) && zeroLocation < ((i+1)*sqrt(puzzleSize))){    //if 0 is between start and end of row
+                possibleMoves.push_back(UP);
+                possibleMoves.push_back(LEFT);
+                possibleMoves.push_back(RIGHT);
+                possibleMoves.push_back(DOWN);
+            }
+        }
+    }
+    
+
+    return possibleMoves;
+}
+
+
+
+
 int main(){
     vector<int> puzzle = {1,2,3,4,5,6,7,8,0};
+    vector<int> puzzle2 = {0,2,3,4,5,6,7,8,1};
+
+    vector<vector<int>> explored;      
 
     linkedList* curNode = createNode(puzzle, 5, NULL);
-    linkedList* curNode2 = createNode(puzzle, 7, NULL);
+    linkedList* curNode2 = createNode(puzzle2, 7, NULL);
     linkedList* curNode3 = createNode(puzzle, 3, NULL);
     priority_queue <linkedList*, vector<linkedList*>, compare> frontier;  
     frontier.push(curNode);
@@ -99,8 +195,29 @@ int main(){
     frontier.pop();
     cout << "last = " << frontier.top()->cost << endl;
 
+    explored.push_back(curNode->state);
+    if(exploredChecker(curNode2->state, explored)){
+        cout << "we explored puzzle2 already" << endl;
+    }
+    else{
+        cout << "we have -not- explored puzzle2 already" << endl;
+    }
+    if(exploredChecker(curNode3->state, explored)){
+        cout << "we explored puzzle already" << endl;
+    }
+    else{
+        cout << "we have -not- explored puzzle already" << endl;
+    }
+
+
+    
+
+
     if(goalChecker(curNode->state, puzzle)){
         cout << "Goal!!" << endl;
+    }
+    else{
+        cout << "no goal" << endl;
     }
 
 
--- uniformCost.h
+++ uniformCost.h
@@ -12,6 +12,11 @@ int maxNodesinQueue;
 bool uniformCostSearch(vector<int>);
 void finalOutput(int, int);
 bool goalChecker(vector<int>, vector<int>);
+bool exploredChecker(vector<int>, vector<vector<int>>);
+
+//test these two when get back
+int findLocationOfZero(vector<int>);
+vector<int> findMoves(int zeroLocation, int puzzleSize);
 
 
 
