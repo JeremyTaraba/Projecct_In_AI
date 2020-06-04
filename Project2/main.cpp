fb3646648745fb127cce8ce9bc5b89ea24e43ffb
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-03 23:08:53 -0700

    Update main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,7 +4,22 @@
 
 using namespace std;
 
+//Create nearest neighbor classifier (keep training data in memory and when new data point is given classify it)
 
+int nearestNeighborClassifier(){
+    
+
+
+
+}
+
+
+
+
+//Create Leave one out validator (need training data and the classifier)
+
+
+//Create Forward search algorithm (adds or removes features and assigns accuracy score to the combinations and chooses highest one)
 
 
 int main(){
