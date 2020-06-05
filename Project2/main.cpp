18005028b2b03fd85bf0d2130b6be5e67b769b1e
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-04 22:56:33 -0700

    Added training data

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -34,7 +35,36 @@ int nearestNeighborClassifier(/* Chosen_Feature_indexes, point*/){
 //Create Forward search algorithm (adds or removes features and assigns accuracy score to the combinations and chooses highest one)
 
 
-//read the data and return a point
+//read the data and return a vector of points
+vector<point> import_data(string filename){
+    //for each line, read in data and create a data point, shove these into a vector of points
+    ifstream infile;
+    infile.open(filename);
+    vector<point> Data;
+    string line;
+    double instance_feature;
+    bool first_feature;
+    if(infile.is_open()){
+        while(getline(infile, line)){
+            point instance;              //create new point
+            istringstream iss(line);    //covert string to any varable type (double in this case)
+            first_feature = true;
+            while(iss >> instance_feature){      //read all values from the file
+                if(first_feature){              //put class into classification
+                    instance.classification = instance_feature;
+                    first_feature = false;
+                }
+                else{
+                    instance.features.push_back(instance_feature);  //put feature into features vector
+                }
+            }
+            Data.push_back(instance);       //put finished instance into vector of data
+        }
+    }
+
+    return Data;
+}
+
 
 
 int main(int argc, char *argv[]){
@@ -55,26 +85,8 @@ int main(int argc, char *argv[]){
             cout << "ERROR: Too many arguments, only include one file of data points" << endl;
         }
     }
-    
-    //for each line, read in data and create a data point, shove these into a vector of points
-    ifstream infile;
-    infile.open(filename);
-    vector<point> Data;
-    string line;
-    double instance_feature;
-    if(infile.is_open()){
-        while(getline(infile, line)){
-            point instance;              //create new point
-            istringstream iss(line);    //covert string to int
-            iss >> instance.classification;  //put class into classification
-            while(iss >> instance_feature){      //put all features into the feature vector
-                instance.features.push_back(instance_feature);
-            }
-            
-            //put finished instance into vector of data
-            
-        }
-    }
+    vector<point> Data = import_data(filename);     //read in data points and store in a vector
+   
 
 
 
