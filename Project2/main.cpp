af460165f4d5907f5fde4fa012ad72412679404c
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-04 19:27:08 -0700

    Update main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <math.h>
 #include <random>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -14,11 +16,13 @@ struct point {
 //need to normalize the untis. Do this by Feature1 = (feature1_value - mean(feature1)) / stddev(feature1)
 //Find the distance with dist between point1 and point2 = sqrt( pow(2,point1(feature1) - point2(feature1)) )
 
-int nearestNeighborClassifier(){
+int nearestNeighborClassifier(/* Chosen_Feature_indexes, point*/){
+    //first find all points (is there a better way? yes, remove instances that obviously wont be considered by sorting)
+    //normalize data for point and other points
+    //then calculate distances and find nearest neighbor
+    //return classification
 
-
-
-
+    return 1;
 }
 
 
@@ -30,9 +34,47 @@ int nearestNeighborClassifier(){
 //Create Forward search algorithm (adds or removes features and assigns accuracy score to the combinations and chooses highest one)
 
 
-int main(){
+//read the data and return a point
 
-    //First read in data and make training set?
+
+int main(int argc, char *argv[]){
+    //First read in data, this is your training set
+    //using leave one out validator, nearest neighbor classifier, and forward search algorithm, find the highest accuracy of feature combinations
+    //return the best features.
+    //cout << "The purpose of this program is to take in a file of classified data points as input and output the best combination of features" << endl;
+
+    string filename;
+    if(argc == 2){
+        filename = argv[1];
+    }
+    else{
+        if(argc < 2){
+            cout << "ERROR: Not enough arguments, include a file of data points" << endl;
+        }
+        else if(argc > 2){
+            cout << "ERROR: Too many arguments, only include one file of data points" << endl;
+        }
+    }
+    
+    //for each line, read in data and create a data point, shove these into a vector of points
+    ifstream infile;
+    infile.open(filename);
+    vector<point> Data;
+    string line;
+    if(infile.is_open()){
+        while(getline(infile, line)){
+            point instance;              //create new point
+            istringstream iss(line);    //covert string to int
+            iss >> instance.classification;  //put class into classification
+            
+           
+            
+            
+            //put all features into the feature vector
+            for()
+        }
+    }
+
 
 
     return 0;
