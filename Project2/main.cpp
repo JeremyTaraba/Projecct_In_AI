718c8b34559df528cfce6bd3278c3b9975af9d2a
JeremyTaraba <jeremytaraba@gmail.com>
2020-06-06 17:29:22 -0700

    mean and standard deviation
    mean and standard deviation work. now to get nearest neighbor to work

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <numeric>
 
 //just for testing
 #include <stdlib.h>     /* srand, rand */
@@ -16,18 +17,72 @@ using namespace std;
 struct point {
     int classification;
     vector<double> features;  
+
 };
 
+//gives us two vectors, one with mean values for each feature and one with stddev for each feature
+vector< vector<double> > mean_and_standardDev(vector<point> Data){
+    //finds the sum
+    vector< vector<double> > mean_stddev;
+    vector<double> sum_vect;
+    double sum = 0;
+    for(int i = 0; i < Data.at(0).features.size(); i++){
+        for(int k = 0; k < Data.size(); k++){
+            sum += Data.at(k).features.at(i);
+        }
+        sum_vect.push_back(sum);
+        sum = 0;
+    }
+    //finds the mean
+    vector<double> mean;
+    for(int i = 0; i < Data.at(0).features.size(); i++){
+        mean.push_back( sum_vect.at(i) / Data.size() );
+    }
+    mean_stddev.push_back(mean);
 
-//Create nearest neighbor classifier (keep training data in memory and when new data point is given classify it)
-//need to normalize the untis. Do this by Feature1 = (feature1_value - mean(feature1)) / stddev(feature1)
-//Find the distance with dist between point1 and point2 = sqrt( pow(2,point1(feature1) - point2(feature1)) )
+    //find the standard deviation
+    double stddev;
+    vector<double> stddev_vect;
+    for(int i = 0; i < Data.at(0).features.size(); i++){
+        for(int k = 0; k < Data.size(); k++){
+            stddev += pow(Data.at(k).features.at(i) - mean.at(i), 2);
+        }
+        stddev_vect.push_back(sqrt( stddev / Data.size()));
+        cout << "for feature " << i << " stddev = " << stddev_vect.at(i) << "    mean = " << mean.at(i) << endl;
+        stddev = 0;
+    }
+    mean_stddev.push_back(stddev_vect);
 
-int nearestNeighborClassifier(vector<int> Chosen_Feature_indexes, point instance, vector<point> test_data){
+    return mean_stddev;
+}
+
+int nearestNeighborClassifier(vector<int> Chosen_Feature_indexes, point instance, vector<point> test_data, vector<double> mean, vector<double> stddev){
     //first find all points (is there a better way? yes, remove instances that obviously wont be considered by sorting)
-    //normalize data for point and other points
-    //then calculate distances and find nearest neighbor
+    //normalize data for a point is Feature1 = (feature1_value - mean(feature1)) / stddev(feature1)
+    //then calculate distances and find nearest neighbor with dist between point1 and point2 = sqrt( pow(2,point1(feature1) - point2(feature1)) )
     //return classification
+
+    int closest_point = 0;      //set closest point to a random point in the test_data
+    vector< vector<double> > normalized_features_test;
+    vector<double> normalized_features_instance;
+    //we need to store x,y and z features in the normalized feature set (columns) go over 1 feature for all points not all features for one point
+    double normalized_feature;
+
+    //create all normalized features for the instance
+    for(int i = 0; i < instance.features.size(); i++){
+        normalized_feature = (instance.features.at(i) - mean.at(i)) / stddev.at(i);
+        normalized_features_instance.push_back(normalized_feature);
+    }
+
+    //create all normalized features for the test set
+    for(int i = 0; i < test_data.at(0).features.size(); i++){
+        for(int k = 0; k < test_data.size(), k++){
+            normalized_feature = (test_data.at(k).features.at(i) - mean.at(i)) / stddev.at(i);
+            normalized_features_test.at(k).push_back(normalized_feature);
+        }
+    }
+
+    
 
     return rand() % 2 + 1;
 }
@@ -36,7 +91,7 @@ int nearestNeighborClassifier(vector<int> Chosen_Feature_indexes, point instance
 
 
 //Create Leave one out validator (need training data and the classifier) 
-float leaveOneOutValidator(vector<point> Data, vector<int> current_features, int feature_to_add){
+float leaveOneOutValidator(vector<point> Data, vector<int> current_features, int feature_to_add, vector<double> mean, vector<double> stddev){
     double accuracy = 0.0;
     vector<int> all_features = current_features;
     all_features.push_back(feature_to_add);
@@ -49,7 +104,7 @@ float leaveOneOutValidator(vector<point> Data, vector<int> current_features, int
     for(int i = 0; i < num_points; i++){
        test_data.erase(test_data.begin() + i);  //erase the leave one out point
        correct_classification = Data.at(i).classification;
-       guessed_classification = nearestNeighborClassifier(all_features, Data.at(i), test_data);
+       guessed_classification = nearestNeighborClassifier(all_features, Data.at(i), test_data, mean, stddev);
        if(correct_classification == guessed_classification){
            correct_class_count++;
        }
@@ -68,6 +123,11 @@ float leaveOneOutValidator(vector<point> Data, vector<int> current_features, int
 vector<int> forwardSearchAlgorithm(vector<point> Data){
     double accuracy = 0;
     vector<int> current_set_of_features;
+
+    //find mean and stddev for normalization
+    vector< vector<double> > mean_stddev = mean_and_standardDev(Data);
+    vector<double> mean = mean_stddev.at(0);
+    vector<double> stddev = mean_stddev.at(1);
     
 
     for(int i = 0; i < Data.at(0).features.size(); i++){
@@ -76,7 +136,7 @@ vector<int> forwardSearchAlgorithm(vector<point> Data){
         
         for(int k = 0; k < Data.at(0).features.size(); k++){
             if(find(current_set_of_features.begin(), current_set_of_features.end(), k) == current_set_of_features.end()){  //if k is not in current features
-                accuracy = leaveOneOutValidator(Data, current_set_of_features, k);
+                accuracy = leaveOneOutValidator(Data, current_set_of_features, k, mean, stddev);
                 if(accuracy > best_accuracy_so_far){
                     best_accuracy_so_far = accuracy;
                     feature_to_add = k+1;
